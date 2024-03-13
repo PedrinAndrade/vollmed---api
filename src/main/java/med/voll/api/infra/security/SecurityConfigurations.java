4153a856cd626e306ae04f2566768c3330b28f2b
PedrinAndrade <pedrohandradeg@gmail.com>
2024-03-13 11:27:04 -0300

    Json Web Token

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,6 +1,5 @@
 package med.voll.api.infra.security;
 
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
